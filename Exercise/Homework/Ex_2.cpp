3aef8ada26c1a6db8702e3b8a001b6b7b1680943
LAS_Chanh <93665501+thanhnguyenhoang171@users.noreply.github.com>
2022-07-07 10:28:19 +0700

    Delete Exercise directory

--- Exercise/Homework/Ex_2.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-#define MAX 100
-struct Solid_list
-{
-    int A[MAX];
-    int n;
-};
-void Input(Solid_list &)
--- Exercise/RW/temp.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-using namespace std;
-float SQRT(int n)
-{
-    
-}
