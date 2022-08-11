74e7c819f3861419ccbbfbce85e27b7a94fae589
LAS_Chanh <93665501+thanhnguyenhoang171@users.noreply.github.com>
2022-08-11 10:39:49 +0700

    Add files via upload
    chapter 2

---
+++ Source.cpp
@@ -0,0 +1,18 @@
+﻿#include <conio.h>
+#include <ctype.h>
+
+int main(void)
+{
+	int ch;
+
+	_cputs("Type 'Y' when finished typing keys: ");
+	do
+	{
+		ch = _getch();
+		ch = toupper(ch);
+	} while (ch != 'Y');
+
+	_putch(ch);
+	_putch('\r');    // Carriage return
+	_putch('\n');    // Line feed
+}
